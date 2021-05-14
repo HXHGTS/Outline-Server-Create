6ef6964d09f25448eece6054b4003f7491b22eac
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-05-15 02:28:30 +0800

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -9,3 +9,5 @@ systemctl enable docker
 curl https://cdn.jsdelivr.net/gh/HXHGTS/TCPOptimization/TCPO.sh | sh
 
 curl https://cdn.jsdelivr.net/gh/Jigsaw-Code/outline-server/src/server_manager/install_scripts/install_server.sh | sh
+
+exit 0
